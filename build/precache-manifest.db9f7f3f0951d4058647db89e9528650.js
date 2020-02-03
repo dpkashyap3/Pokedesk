3951e707b961ab61ae7520bc3db6a65142744df0
dpkashyap3 <32429850+dpkashyap3@users.noreply.github.com>
2020-02-03 23:47:10 +0530

    Second

---
+++ build/precache-manifest.db9f7f3f0951d4058647db89e9528650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130128bf6e37c34b1f8779999faa61a2",
+    "url": "/pokedesk/index.html"
+  },
+  {
+    "revision": "ec621b3cbc52c4a2d29a",
+    "url": "/pokedesk/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "1823601f093fceabca38",
+    "url": "/pokedesk/static/css/main.77b38919.chunk.css"
+  },
+  {
+    "revision": "ec621b3cbc52c4a2d29a",
+    "url": "/pokedesk/static/js/2.af3c1d21.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/pokedesk/static/js/2.af3c1d21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1823601f093fceabca38",
+    "url": "/pokedesk/static/js/main.989bb0b7.chunk.js"
+  },
+  {
+    "revision": "ae3ca1a6f3ced74649be",
+    "url": "/pokedesk/static/js/runtime-main.33bf08ca.js"
+  }
+]);
